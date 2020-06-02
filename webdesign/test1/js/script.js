b46ad610ac9c4048efc6ff1a875bf5cf003fc6f7
HwangOonha <dnsgk4983@gmail.com>
2020-06-02 13:16:22 +0900

    0602

--- webdesign/test1/js/script.js
+++ webdesign/test1/js/script.js
@@ -7,19 +7,31 @@ $(function(){
         $(this).children(".subNav").stop().slideUp();
     });
     setInterval(function(){
-        $(".slide-wrap").delay("2500");
-        $(".slide-wrap").animate({marginTop: "-300px"},"500");
-        $(".slide-wrap").delay("2500");
-        $(".slide-wrap").animate({maiginTop: "-600px"},"500");
-        $(".slide-wrap").delay("2500");
-        $(".slide-wrap").animate({marginTop: "0"},"500");
+        $(".slider-wrap").delay("2500");
+        $(".slider-wrap").animate({marginTop: "-300px"},"500");
+        $(".slider-wrap").delay("2500");
+        $(".slider-wrap").animate({marginTop: "-600px"},"500");
+        $(".slider-wrap").delay("2500");
+        $(".slider-wrap").animate({marginTop: "0"},"500");
     });
     $("#btn1").click(function(){
         $(".tab1-conts1").css("display","flex");
+        $("#btn1").css("background","none");
+        $("#btn1").css("borderBottom","none");
+        $("#btn2").css("background","rgba(255,255,255,0.5)");
         $(".tab1-conts2").css("display","none");
     });
     $("#btn2").click(function(){
         $(".tab1-conts2").css("display","flex");
+        $("#btn2").css("background","none");
+        $("#btn2").css("borderBottom","none");
+        $("#btn1").css("background","rgba(255,255,255,0.5)");
         $(".tab1-conts1").css("display","none");
+    });
+    $("#open").click(function(){
+        $(".popup").fadeIn();
+    });
+    $(".close").click(function(){
+        $(".popup").fadeOut();
     });
 });
