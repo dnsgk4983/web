28b4a95ebe9831502b381ceb46bb3470edb4187a
HwangOonha <dnsgk4983@gmail.com>
2020-06-02 12:41:50 +0900

    0602

--- webdesign/test1/js/script.js
+++ webdesign/test1/js/script.js
@@ -8,16 +8,15 @@ $(function(){
     });
     setInterval(function(){
         $(".slide-wrap").delay("2500");
-        $(".slide-wrap").animate({marginTop: "-300"},"500");
+        $(".slide-wrap").animate({marginTop: "-300px"},"500");
         $(".slide-wrap").delay("2500");
-        $(".slide-wrap").animate({maiginTop: "-600"},"500");
+        $(".slide-wrap").animate({maiginTop: "-600px"},"500");
         $(".slide-wrap").delay("2500");
         $(".slide-wrap").animate({marginTop: "0"},"500");
     });
     $("#btn1").click(function(){
         $(".tab1-conts1").css("display","flex");
-        
-        $(".tab-conts2").css("display","none");
+        $(".tab1-conts2").css("display","none");
     });
     $("#btn2").click(function(){
         $(".tab1-conts2").css("display","flex");
